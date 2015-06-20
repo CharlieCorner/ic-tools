3fe614fdd37bbea5f0288ba30463fd70463d960e
Charles Corner <ccorner.dev@gmail.com>
2015-06-20 18:07:24 -0400

    Added the templates using the new import method in HTML5

--- javascripts/sql-quick-generator.js
+++ javascripts/sql-quick-generator.js
@@ -1,14 +1,17 @@
 Generator = (function(){
 	
-	var SQL_TEMPLATES_LOCATION = "/templates/sql-templates/sql-templates.html";
-	var HB_TEMPLATES_LOCATION = "/templates/handlebars-templates/templates.html";
+	var SQL_TEMPLATES_LOCATION = "templates/sql-templates/sql-templates.html";
+	var HB_TEMPLATES_LOCATION = "templates/handlebars-templates/templates.html";
 	
 	var sayHi = function(){
 		console.log("HIII!");
 	};
 	
 	var includeHtml = function(sId, sLocation){
-		$(sId).load(sLocation);
+		$.get(sLocation, function(data) {
+			$(sId).html(data);
+		});
+		//$(sId).load(sLocation);
 	};
 	
 	var loadSqlTemplates = function() {
